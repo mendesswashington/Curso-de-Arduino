dcb3d5d09cd989d0068ab81022b11af4d5492845
mendesswashington <mendesswashington@gmail.com>
2017-10-08 23:20:56 -0300

    Add files via upload

---
+++ LM35/LM35.ino
@@ -0,0 +1,20 @@
+/*
+ *Curso de Arduino
+ * Washington Santos
+*/
+
+const int LM35 = 0;
+float temperatura = 0;
+int ADClido = 0;
+void setup(){
+ Serial.begin(9600);
+ analogReference(INTERNAL); //Se estiver usando Arduino Mega, use INTERNAL1V1
+}
+void loop(){
+ ADClido = analogRead(LM35);
+ temperatura = ADClido * 0.1075268817204301;
+ Serial.print("Temperatura = ");
+ Serial.print(temperatura);
+ Serial.println(" *C");
+ delay(1000);
+} 

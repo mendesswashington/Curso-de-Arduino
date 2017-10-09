50d541b959af3ce554118ce8eb3e5a8241004adf
mendesswashington <mendesswashington@gmail.com>
2017-10-08 22:28:23 -0300

    Add files via upload

---
+++ BtnStatus/BtnStatus.ino
@@ -0,0 +1,27 @@
+#define led 13
+#define button 7
+byte val = 0;
+boolean status1 = false;
+
+void setup() {
+  pinMode(led, OUTPUT);
+  pinMode(button, INPUT);
+
+}
+
+void loop() {
+  val = digitalRead(button);
+
+  if (val == HIGH) {
+    if (status1) {
+      digitalWrite(led, LOW);
+      Serial.println("off");
+      status1 = false;
+    } else {
+      digitalWrite(led, HIGH);
+      Serial.println("on");
+      status1 = true;
+    }
+    delay(250);
+  }
+}

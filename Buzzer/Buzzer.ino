851f4532fd480c4ab6603e68b103cb047dc2ef43
mendesswashington <mendesswashington@gmail.com>
2017-10-08 23:14:46 -0300

    Add files via upload

---
+++ Buzzer/Buzzer.ino
@@ -0,0 +1,16 @@
+/*
+ * Curso de Arduino
+ * Washington Santos
+*/
+const int buzzer = 9; 
+
+void setup() {
+  pinMode(buzzer, OUTPUT);
+}
+
+void loop() {
+  tone(buzzer, 1000);
+  delay(1000);
+  noTone(buzzer);
+  delay(1000);
+}

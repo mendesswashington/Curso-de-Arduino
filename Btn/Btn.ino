da4c33e7a516387b6174bf6f65811cb74f9f0a35
mendesswashington <mendesswashington@gmail.com>
2017-10-08 22:45:04 -0300

    Add files via upload

---
+++ Btn/Btn.ino
@@ -0,0 +1,21 @@
+/*
+ * Curso de Arduino
+ * Washington Santos
+*/
+#define LED 13
+#define BTN 7
+
+void setup() {
+ pinMode(LED, OUTPUT);
+ pinMode(BTN, INPUT);
+}
+
+void loop() {
+ byte status1 = digitalRead(BTN);
+  if(status1){
+    digitalWrite(LED, HIGH);
+  }else{
+    digitalWrite(LED, LOW);
+  }
+  delay(250);
+}

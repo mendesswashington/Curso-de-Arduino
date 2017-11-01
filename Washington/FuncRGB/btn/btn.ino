fe1a686a8de465a99a4fc5736182d3702e5c9325
mendesswashington <mendesswashington@gmail.com>
2017-11-01 13:08:03 -0200

    Add files via upload

---
+++ Washington/FuncRGB/btn/btn.ino
@@ -0,0 +1,21 @@
+#define LED 13
+#define BTN 7
+byte status1 = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED,OUTPUT);
+  pinMode(BTN,INPUT);
+  digitalWrite(LED,LOW);
+}
+
+void loop() {
+  status1 = digitalRead(BTN);
+
+  if(status1){
+    digitalWrite(LED, HIGH);
+  }else{
+    digitalWrite(LED, LOW);
+  }
+  delay(250);
+}
---
+++ Washington/FuncRGB/btnArray/btnArray.ino
@@ -0,0 +1,32 @@
+const int LED[3] = {13, 12, 11}; //Array para as potas dos leds
+const int BTN[3] = {4, 3, 2}; // Array para os butões
+      int vet[3] = {0, 0 , 0}; //Array para armazenar status dos butões
+
+void setup() {
+  /*
+     Inicia o programa e declara os pinos dos leds como saida
+    e o dos butões como entrada
+  */
+  Serial.begin(9600);
+  for (int i = 0; i < 3; i++) {
+    pinMode(LED[i], OUTPUT);
+    pinMode(BTN[i], INPUT);
+    digitalWrite(LED[i], LOW);
+  }
+}
+
+void loop() {
+  for (int i = 0; i < 3; i++) {
+    std[i] = digitalRead(BTN[i]);
+  }
+  for (int i = 0; i < 3; i++) {
+    if (std[i] == LED[i]) {
+      digitalWrite(LED[i], HIGH);
+      //Serial.println("LED["+i +"] = HIGH");
+    } else {
+      digitalWrite(LED[i], LOW);
+      //Serial.println("LED["+i+"] = LOW");
+    }
+    delay(250);
+  }
+}

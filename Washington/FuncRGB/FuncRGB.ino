da1584374195a4c03887d716c542be699e63edb1
mendesswashington <mendesswashington@gmail.com>
2017-11-01 13:52:01 -0200

    Add files via upload

---
+++ Washington/FuncRGB/FuncRGB.ino
@@ -0,0 +1,26 @@
+/*
+ * Curso de arduino 2017
+ * Washington Santos
+*/
+
+const int LED[] = {9, 10, 11};
+int i, w[3] = {255,255,255};
+void RGB(int COR[]);
+void setup() {
+  // put your const setup code here, to run once:
+  Serial.begin(9600);
+  for(i = 0; i < 3; i++){
+    pinMode(LED[i],OUTPUT);
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  RGB(w);
+}
+
+void RGB(int COR[]){
+  for(i = 0; i < 3; i++){
+    analogWrite(LED[i],COR[i]);
+  }
+}
